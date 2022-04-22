6924db7fc59a71f5e74d7d35d6456a863db9cf28
Benjamin Hills <benjaminhhills@gmail.com>
2022-04-21 17:25:33 -0700

    Filling in the numerical model testing

--- iceotherm/tests/numerical_test.py
+++ iceotherm/tests/numerical_test.py
@@ -41,6 +41,19 @@ class TestNumericalFunctions(unittest.TestCase):
         m.source_terms()
         m.stencil(dt=1.*const.spy)
 
+    def test_cfl(self):
+        m = ice_temperature(adot=1.)
+        m.initial_conditions()
+        m.source_terms()
+        m.stencil(dt='CFL')
+        with self.assertRaises(ValueError) as context:
+            m.stencil(dt=1000.*const.spy)
+
+    def test_stencil_no_dt(self):
+        m = ice_temperature()
+        with self.assertRaises(ValueError) as context:
+            m.stencil()
+
     def test_steadystate(self):
         m = ice_temperature()
         m.initial_conditions()
@@ -50,20 +63,18 @@ class TestNumericalFunctions(unittest.TestCase):
 
     def test_run(self):
         ntsteps = 1000
-        adot = .1
-        Ts = -25.
-        H = 1000.
-        m = ice_temperature(Ts=Ts,adot=adot,H=H)
+        m = ice_temperature(Ts=-25.,adot=.1,H=1000.)
         m.ts = np.arange(ntsteps)*const.spy
         m.Ts_s = [m.Ts]*ntsteps
         m.adot_s = [m.adot]*ntsteps
-        m.Hs = np.linspace(H,H+10,ntsteps)
+        m.Hs = np.linspace(1000,1010,ntsteps)
         m.initial_conditions()
         m.source_terms()
         m.stencil()
         m.flags.append('verbose')
         m.flags.append('save_all')
         m.flags.append('temp-dependent')
+        m.flags.append('vertical_shear')
         m.k = conductivity(m.T.copy(),m.rho)
         m.Cp = heat_capacity(m.T.copy())
         m.numerical_transient()
@@ -93,6 +104,33 @@ class TestNumericalFunctions(unittest.TestCase):
         m.source_terms()
         m.stencil()
         m.numerical_transient()
+
+    def test_shear_margin(self):
+        m = ice_temperature(Ts=-25.,adot=.05,H=500.,qgeo=.1)
+        ntsteps = 100
+        m.ts = np.arange(ntsteps)*const.spy
+        m.Ts_s = [m.Ts]*ntsteps
+        m.adot_s = [m.adot]*ntsteps
+        m.flags.append('plane_strain')
+        m.flags.append('long_advection')
+        m.dTs = 1e-6
+        m.da = 1e-7
+        m.dH = 1e-3
+        m.Uslide = 100./const.spy
+        m.Udef = 100./const.spy
+        m.initial_conditions()
+        m.source_terms()
+        m.stencil()
+
+        m.Axy_init = 'full'
+        m.numerical_transient()
+
+        m.Axy_init = 'temperate'
+        m.numerical_transient()
+
+        m.Axy_init = 2e-24
+        m.numerical_transient()
+
 
 if __name__ == '__main__':
     unittest.main()
